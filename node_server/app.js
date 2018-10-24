97ee81498e0688d205ef39d2fdb67eaff28eb189
alix <alixgonnot@gmail.com>
2018-10-24 21:59:37 +0200

    server code

---
+++ node_server/app.js
@@ -0,0 +1,57 @@
+// app.js
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/node_modules'));
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(client) {
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+    });
+  });
+
+server.listen(4200);
+
+/*
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+  // process HTTP request. Since we're writing just WebSockets
+  // server we don't have to implement anything.
+});
+server.listen(1337, function() {
+  console.log("I AM ONLINE !!!!! ");
+ });
+
+// create the server
+wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+// WebSocket server
+wsServer.on('request', function(request) {
+  var connection = request.accept(null, request.origin);
+  console.log("Guys, there is something out there :/ ...");
+
+  // This is the most important callback for us, we'll handle
+  // all messages from users here.
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log(message);
+      // process WebSocket message
+    }
+  });
+
+  connection.on('close', function(connection) {
+    // close user connection
+  });
+});
+*/
